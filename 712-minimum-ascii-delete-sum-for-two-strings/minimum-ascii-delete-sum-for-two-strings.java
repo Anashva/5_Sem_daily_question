dfcfad61f64732ba1a15c57b4ee5a13f95cebdf1
Anashva Singh <anashva2406@gmail.com>
2025-09-06 18:20:27 +0530

    Time: 20 ms (66.33%) | Memory: 44.9 MB (68.72%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,39 +1,40 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
         // return deletesum(s1,s2,0,0);
-        int[][] dp=new int[s1.length()][s2.length()];
-        for(int[] a:dp){
-            Arrays.fill(a,-1);
-        }
-        return memo(s1,s2,0,0,dp);
+        // int[][] dp=new int[s1.length()][s2.length()];
+        // for(int[] a:dp){
+        //     Arrays.fill(a,-1);
+        // }
+        // return memo(s1,s2,0,0,dp);
+        return BU(s1,s2);
     }
-    // public int deletesum(String s1,String s2,int i,int j){
-    //     if(i==s1.length()){
-    //         int sum=0;
-    //         for(int k=j;k<s2.length();k++){
-    //             sum+=s2.charAt(k);
-    //         }
-    //         return sum;
-    //     }
-    //     if(j==s2.length()){
-    //         int sum=0;
-    //         for(int k=i;k<s1.length();k++){
-    //             sum+=s1.charAt(k);
-    //         }
-    //         return sum;
-    //     }
+    public int deletesum(String s1,String s2,int i,int j){
+        if(i==s1.length()){
+            int sum=0;
+            for(int k=j;k<s2.length();k++){
+                sum+=s2.charAt(k);
+            }
+            return sum;
+        }
+        if(j==s2.length()){
+            int sum=0;
+            for(int k=i;k<s1.length();k++){
+                sum+=s1.charAt(k);
+            }
+            return sum;
+        }
 
-    //     int ans=0;
-    //     if(s1.charAt(i)==s2.charAt(j)){
-    //         ans=deletesum(s1,s2,i+1,j+1);
-    //     }
-    //     else{
-    //         int l=s1.charAt(i)+deletesum(s1,s2,i+1,j);
-    //         int r=s2.charAt(j)+deletesum(s1,s2,i,j+1);
-    //         ans+=Math.min(l,r);
-    //     }
-    //     return ans;
-    // }
+        int ans=0;
+        if(s1.charAt(i)==s2.charAt(j)){
+            ans=deletesum(s1,s2,i+1,j+1);
+        }
+        else{
+            int l=s1.charAt(i)+deletesum(s1,s2,i+1,j);
+            int r=s2.charAt(j)+deletesum(s1,s2,i,j+1);
+            ans+=Math.min(l,r);
+        }
+        return ans;
+    }
 
     public int memo(String s1,String s2,int i,int j,int[][] dp){
         if(i==s1.length()){
@@ -69,7 +70,7 @@ class Solution {
         for(int i=1;i<dp.length;i++){
             dp[i][0]=dp[i-1][0]+(int)s1.charAt(i-1);
         }
-        for(int i=1;i<=dp[0].length;i++){
+        for(int i=1;i<dp[0].length;i++){
             dp[0][i]=dp[0][i-1]+(int)s2.charAt(i-1);
         }
         for(int i=1;i<dp.length;i++){
