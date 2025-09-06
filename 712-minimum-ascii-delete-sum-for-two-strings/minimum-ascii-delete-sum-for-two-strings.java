a956d7f92bf548c112083096272facb0a7d93f74
Anashva Singh <anashva2406@gmail.com>
2025-09-06 18:19:27 +0530

    Time: 31 ms (10.68%) | Memory: 44.6 MB (90.95%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -64,4 +64,24 @@ class Solution {
         }
         return dp[i][j]=ans;
     }
+    public int BU(String s1,String s2){
+        int[][] dp=new int[s1.length()+1][s2.length()+1];
+        for(int i=1;i<dp.length;i++){
+            dp[i][0]=dp[i-1][0]+(int)s1.charAt(i-1);
+        }
+        for(int i=1;i<=dp[0].length;i++){
+            dp[0][i]=dp[0][i-1]+(int)s2.charAt(i-1);
+        }
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.min((int)s1.charAt(i-1)+dp[i-1][j],(int)s2.charAt(j-1)+dp[i][j-1]);
+                }
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
 }
