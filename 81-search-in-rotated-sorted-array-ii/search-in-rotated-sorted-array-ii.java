bf00799fdaa0c73241ce058d3eb3c2b6a0db5c64
Anashva Singh <anashva2406@gmail.com>
2025-09-26 14:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (83.20%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,43 +1,38 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int i:nums){
-            if(target==i){
+        int left=0;
+        int right=nums.length-1;
+        int start=0;
+        int end=nums.length-1;
+        int mid=0;
+        while(start<=end){
+            mid=start+(mid-start)/2;
+            if(nums[mid]==target){
                 return true;
             }
-        }
-        // int left=0;
-        // int right=nums.length-1;
-        // int start=0;
-        // int end=nums.length-1;
-        // int mid=0;
-        // while(start<=end){
-        //     mid=start+(mid-start)/2;
-        //     if(nums[mid]==target){
-        //         return true;
-        //     }
-        //     else{
-        //         if(nums[left]<nums[mid]){
-        //             if(target>=nums[left] && target<nums[mid]){
-        //                 end=mid-1;
-        //         }
-        //             else{
-        //                 start=mid+1;
-        //         }
-        //         }
-        //         else if(nums[right]>nums[mid]){
-        //             if(target<=nums[right] && target>nums[mid]){
-        //                 start=mid+1;
-        //             }
-        //             else{
-        //                 end=mid-1;
-        //             }
+            else{
+                if(nums[left]<nums[mid]){
+                    if(target>=nums[left] && target<nums[mid]){
+                        end=mid-1;
+                }
+                    else{
+                        start=mid+1;
+                }
+                }
+                else if(nums[right]>nums[mid]){
+                    if(target<=nums[right] && target>nums[mid]){
+                        start=mid+1;
+                    }
+                    else{
+                        end=mid-1;
+                    }
 
-        //         }
-        //         else{
-        //             start++;
-        //         }
-        //     }
-        // }
+                }
+                else{
+                    start++;
+                }
+            }
+        }
         return false;
     }
 }
