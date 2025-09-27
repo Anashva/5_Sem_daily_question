34d1c943344803814030e1550d449137919d4d78
Anashva Singh <anashva2406@gmail.com>
2025-09-27 14:03:39 +0530

    Time: 13 ms (67.39%) | Memory: 44.8 MB (96.65%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -4,16 +4,28 @@ class Solution {
         for(int i=0;i<s.length();i++){
             mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
         }
-        List<Map.Entry<Character,Integer>> ll=new ArrayList<>(mp.entrySet());
-        Collections.sort(ll,(a,b)->b.getValue()-a.getValue());
+        PriorityQueue<Map.Entry<Character,Integer>> pq=new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+        pq.addAll(mp.entrySet());
         StringBuilder sb=new StringBuilder();
-        for(Map.Entry<Character,Integer> arr:ll){
-            char ch=arr.getKey();
-            int c=arr.getValue();
+        while(!pq.isEmpty()){
+            Map.Entry<Character,Integer> ll=pq.poll();
+            char ch=ll.getKey();
+            int c=ll.getValue();
             for(int i=0;i<c;i++){
                 sb.append(ch);
             }
         }
         return sb.toString();
+        // List<Map.Entry<Character,Integer>> ll=new ArrayList<>(mp.entrySet());
+        // Collections.sort(ll,(a,b)->b.getValue()-a.getValue());
+        // StringBuilder sb=new StringBuilder();
+        // for(Map.Entry<Character,Integer> arr:ll){
+        //     char ch=arr.getKey();
+        //     int c=arr.getValue();
+        //     for(int i=0;i<c;i++){
+        //         sb.append(ch);
+        //     }
+        // }
+        // return sb.toString();
     }  
 }
