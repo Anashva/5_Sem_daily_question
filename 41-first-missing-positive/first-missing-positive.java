6aafd53824bbf16f477636ea15625a338a42698a
Anashva Singh <anashva2406@gmail.com>
2025-09-27 15:37:47 +0530

    Time: 1 ms (100.00%) | Memory: 57.5 MB (33.27%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,18 +1,48 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-      HashSet<Integer> mp=new HashSet<>();
-      for(int i:nums){
-        mp.add(i);
-      }
-       int c=1;
-       for(int k:mp){
-        if(mp.contains(c)){
-            c++;
+        boolean flag=false;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                flag=true;
+            }
+            if(nums[i]<=0 || nums[i]>nums.length){
+                nums[i]=1;
+            }
         }
-        else{
-            return c;
+        if(!flag){
+            return 1;
         }
-       }
-       return c;
+        for(int i=0;i<nums.length;i++){
+            int id=Math.abs(nums[i])-1;
+           if(nums[id]>0){
+            nums[id]=-nums[id];
+           }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                return i+1;
+            }
+        }
+        return nums.length+1;
+
+
+
+
+
+
+    //   HashSet<Integer> mp=new HashSet<>();
+    //   for(int i:nums){
+    //     mp.add(i);
+    //   }
+    //    int c=1;
+    //    for(int k:mp){
+    //     if(mp.contains(c)){
+    //         c++;
+    //     }
+    //     else{
+    //         return c;
+    //     }
+    //    }
+    //    return c;
     }
 }
