92fa843057a5f587a34ce4527dddb4d878becf53
Anashva Singh <anashva2406@gmail.com>
2025-09-29 15:49:12 +0530

    Time: 8 ms (99.44%) | Memory: 65 MB (5.37%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,40 +1,30 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int len=nums.length;
-        int n=len-k+1;
-        int [] arr=new int[n];
-        // int idx=0;
-        // for(int i=0;i<=len-k;i++){
-        //     PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        //     for(int j=i;j<i+k;j++){
-        //         pq.add(nums[j]);
-        //     }
-        //     arr[idx++]=pq.poll();
-        //     pq.clear();
-        // }
-        // return arr;
-
-
-        Deque<Integer> pq=new ArrayDeque<>();//decreasing order me value ko store karega
-        for (int i = 0; i < k; i++) {
-             while (!pq.isEmpty() && nums[i] >= nums[pq.peekLast()]) 
-             {
-                pq.pollLast();
-             }
-             pq.offerLast(i);
+       int[] prefix=new int[nums.length];
+       int [] suffix=new int[nums.length];
+       int[] ans=new int[nums.length-k+1];
+       prefix[0]=nums[0];
+       for(int i=1;i<nums.length;i++){
+        if(i%k==0){
+            prefix[i]=nums[i];
         }
-        arr[0] = nums[pq.peekFirst()];
-        for(int i=k;i<nums.length;i++){
-            if(!pq.isEmpty() && pq.peekFirst()<=i-k){
-                pq.removeFirst();//condition for index out of window true then remove from starting of queue
-            }
-            while(!pq.isEmpty() && nums[i]>nums[pq.peekLast()]){
-                pq.removeLast();//agr hamra last element chota h current se to remove kr denge
-            }
-            pq.addLast(i);
-            arr[i-k+1]=nums[pq.peekFirst()];
+        else{
+            prefix[i]=Math.max(prefix[i-1],nums[i]);
         }
-        return arr;
+       }
+       suffix[nums.length-1]=nums[nums.length-1];
+       for(int i=nums.length-2;i>=0;i--){
+        if((i+1)%k==0){
+            suffix[i]=nums[i];
+        }
+        else{
+            suffix[i]=Math.max(suffix[i+1],nums[i]);
+        }
+       }
+       for(int i=0;i<ans.length;i++){
+        ans[i]=Math.max(prefix[i+k-1],suffix[i]);
+       }
+       return ans;
 
     }
 
