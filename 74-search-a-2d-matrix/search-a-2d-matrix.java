ab6d270458fa62cee34181aeb04ddaf4f4f894aa
Anashva Singh <anashva2406@gmail.com>
2025-09-29 20:54:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (39.05%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,36 +1,28 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // TC->
-        // for(int i=0;i<matrix.length;i++){
-        //     int l=0;
-        //     int h=matrix[0].length-1;
-        //     while(l<=h){
-        //         int mid=l+(h-l)/2;
-        //         if(matrix[i][mid]==target){
-        //             return true;
-        //         }
-        //         else if(matrix[i][mid]<target){
-        //             l=mid+1;
-        //         }
-        //         else{
-        //             h=mid-1;
-        //         }
-        //     }
-        // }
-        // return false;
-        int n=0;
-        int m=matrix[0].length-1;
-        while(n<matrix.length && m>=0){
-            if(matrix[n][m]==target){
+        // TC->log(n*m)
+        // SC->O(1)
+        int m=matrix[0].length;
+        int n=matrix.length;
+        int l=0;
+        int h=m*n-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            int r=mid/m;
+            int c=mid%m;
+            if(matrix[r][c]==target){
                 return true;
             }
-            if(matrix[n][m]>target){
-                m--;
+            else if(matrix[r][c]>target){
+                h=mid-1;
             }
             else{
-                n++;
+
+                l=mid+1;
             }
         }
+        
         return false;
+        
     }
 }
