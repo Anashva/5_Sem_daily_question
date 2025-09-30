bea847c9b8766ac97148d49ca64fb35ea13f7233
Anashva Singh <anashva2406@gmail.com>
2025-09-30 21:41:49 +0530

    Time: 30 ms (28.73%) | Memory: 45 MB (20.82%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -34,33 +34,44 @@
 
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        // TC = O(n+m)
-        // SC=O(1)
-        if(s2.length()<s1.length()){
+        if(s1.length() > s2.length()) {
             return false;
         }
-        int[]  freq1=new int[26];
-        for(int i=0;i<s1.length();i++){
-            freq1[s1.charAt(i)-'a']++;
+        int k = s1.length();
+        int[] freq = new int[26];
+        for(char c: s1.toCharArray()) {
+            freq[c-'a']++;
         }
-        for(int i=0;i<=s2.length()-s1.length();i++){
-            String s=s2.substring(i,i+s1.length());
-            int[] freq2=new int[26];
-            for(int j=0;j<s.length();j++){
-                freq2[s.charAt(j)-'a']++;
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < 26; i++) {
+            str.append(freq[i]).append('/');
+        }
+        s1 = str.toString();
+        str.setLength(0);
+        Arrays.fill(freq, 0);
+        for(int i = 0; i < k; i++) {
+            freq[s2.charAt(i)-'a']++;
+        }
+        for(int i = 0; i < 26; i++) {
+            str.append(freq[i]).append('/');
+        }
+        String s = str.toString();
+        if(s.equals(s1)) {
+            return true;
+        }
+        str.setLength(0);
+        for(int i = k; i < s2.length(); i++) {
+            freq[s2.charAt(i)-'a']++;
+            freq[s2.charAt(i-k)-'a']--;
+            for(int j = 0; j < 26; j++) {
+                str.append(freq[j]).append('/');
             }
-            if(permute(freq1,freq2)){
+            s = str.toString();
+            if(s.equals(s1)) {
                 return true;
             }
+            str.setLength(0);
         }
         return false;
-    }
-    public boolean permute(int[] freq1,int[] freq2){
-        for(int i=0;i<26;i++){
-            if(freq1[i]<freq2[i]){
-                return false;
-            }
-        }
-        return true;
     }
 }
