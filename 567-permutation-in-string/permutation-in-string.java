a31b7187029f88e3d1e5de46de65c7ee21969093
Anashva Singh <anashva2406@gmail.com>
2025-09-30 19:50:29 +0530

    Time: 77 ms (17.22%) | Memory: 45.5 MB (7.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,27 +1,27 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-       if(s1.length()>s2.length()){
+        if(s2.length()<s1.length()){
+            return false;
+        }
+        int[]  freq1=new int[26];
+        for(int i=0;i<s1.length();i++){
+            freq1[s1.charAt(i)-'a']++;
+        }
+        for(int i=0;i<=s2.length()-s1.length();i++){
+            String s=s2.substring(i,i+s1.length());
+            int[] freq2=new int[26];
+            for(int j=0;j<s.length();j++){
+                freq2[s.charAt(j)-'a']++;
+            }
+            if(permute(freq1,freq2)){
+                return true;
+            }
+        }
         return false;
-       }
-       int[] freq1=new int[26];
-       for(int i=0;i<s1.length();i++){
-        freq1[s1.charAt(i)-'a']++;
-       }
-       for(int i=0;i<=s2.length()-s1.length();i++){
-        int[] freq=new int[26];
-        String s=s2.substring(i,i+s1.length());
-        for(int j=0;j<s.length();j++){
-            freq[s.charAt(j)-'a']++;
-        }
-        if(check(freq1,freq)){
-            return true;
-        }
-       }
-       return false;
     }
-    public boolean check(int[] freq1,int [] freq){
+    public boolean permute(int[] freq1,int[] freq2){
         for(int i=0;i<26;i++){
-            if(freq1[i]!=freq[i]){
+            if(freq1[i]<freq2[i]){
                 return false;
             }
         }
