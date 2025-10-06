eee6729bd4c1773688c70e1a0c0cc3aa10d29293
Anashva Singh <anashva2406@gmail.com>
2025-10-06 11:47:26 +0530

    Time: 3 ms (95.17%) | Memory: 45.1 MB (64.48%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,48 +1,31 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        return Min_OpsBU(word1,word2);
+        int[][] dp=new int[word1.length()][word2.length()];
+        for(int[] a:dp){
+            Arrays.fill(a,-1);
+        }
+        return dis(word1,word2,0,0,dp);
     }
-    public int Min_OpsBU(String s,String t){
-        int[][] dp=new int[s.length()+1][t.length()+1];
-        for(int i=1;i<dp.length;i++){
-            dp[i][0]=i;
+    public int dis(String w1,String w2,int i,int j,int[][]dp){
+        if(i==w1.length()){
+            return w2.length()-j;
         }
-        for(int i=1;i<dp[0].length;i++){
-            dp[0][i]=i;
+        if(j==w2.length()){
+            return w1.length()-i;
         }
-        for(int i=1;i<dp.length;i++){
-            for(int j=1;j<dp[0].length;j++){
-                if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
-                else{
-                    int d=dp[i-1][j];
-                    int r=dp[i-1][j-1];
-                    int I=dp[i][j-1];
-                    dp[i][j]=Math.min(d,Math.min(I,r))+1;
-                }
-            }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        return dp[dp.length-1][dp[0].length-1];
-    }
-    public static int Min_Ops(String s,String t,int i,int j){
-        if(i==s.length()){
-            return t.length()-j;
-        }
-        if(j==t.length()){
-            return s.length()-i;
-        }
-
         int ans=0;
-        if(s.charAt(i)==t.charAt(j)){
-            ans=Min_Ops(s,t,i+1,j+1);
+        if(w1.charAt(i)==w2.charAt(j)){
+            ans=dis(w1,w2,i+1,j+1,dp);
         }
         else{
-            int d=Min_Ops(s,t,i+1,j);
-            int r=Min_Ops(s,t,i+1,j+1);
-            int I=Min_Ops(s,t,i,j+1);
-            ans=Math.min(d,Math.min(I,r))+1;
+            int I=dis(w1,w2,i,j+1,dp);
+            int D=dis(w1,w2,i+1,j,dp);
+            int R=dis(w1,w2,i+1,j+1,dp);
+            ans=Math.min(I,Math.min(R,D))+1;
         }
-        return ans;
+        return dp[i][j]=ans;
     }
 }
