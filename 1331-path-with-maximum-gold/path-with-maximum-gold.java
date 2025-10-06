271da977e37ed4ad7c12888e553cdba33e69806a
Anashva Singh <anashva2406@gmail.com>
2025-10-06 14:59:44 +0530

    Time: 61 ms (79.92%) | Memory: 41 MB (95.94%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,29 +1,26 @@
 class Solution {
     public int getMaximumGold(int[][] grid) {
-        int ans=0;
+        int ans=Integer.MIN_VALUE;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                ans=Math.max(ans,search(grid,i,j,grid.length,grid[0].length));
+                ans=Math.max(ans,path(grid,i,j));
             }
         }
-
         return ans;
     }
-    public int search(int[][] grid,int cr,int cc,int er,int ec){
-        if(cr<0 || cc<0 || cr==er || cc==ec || grid[cr][cc]==0){
+    public int path(int[][] grid,int cr,int cc){
+        if(cr<0 || cr>=grid.length || cc<0 || cc>=grid[0].length || grid[cr][cc]==0){
             return 0;
         }
-        int ans=0;
         int val=grid[cr][cc];
+        int ans=0;
         grid[cr][cc]=0;
-
-        int a=search(grid,cr+1,cc,er,ec);
-        int b=search(grid,cr,cc+1,er,ec);
-        int c=search(grid,cr-1,cc,er,ec);
-        int d=search(grid,cr,cc-1,er,ec);
-        
+        int l=path(grid,cr,cc-1);
+        int r=path(grid,cr,cc+1);
+        int u=path(grid,cr-1,cc);
+        int d=path(grid,cr+1,cc);
+        ans=Math.max(l,Math.max(r,Math.max(u,d)));
         grid[cr][cc]=val;
-        ans=Math.max(Math.max(a,b),Math.max(c,d));
         return ans+val;
     }
 }
