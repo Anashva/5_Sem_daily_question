7b2f817c30cd98f794f301bdfc5c944ee386e104
Anashva Singh <anashva2406@gmail.com>
2025-10-07 15:47:04 +0530

    Time: 32 ms (42.36%) | Memory: 51.1 MB (67.35%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,21 +1,18 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<double[]> pq=new PriorityQueue<>(Comparator.comparingDouble(a->a[2]));
         int[][] ans=new int[k][2];
-        PriorityQueue<double[]> pq=new PriorityQueue<>(Comparator.comparingDouble(a -> a[2]));
         for(int i=0;i<points.length;i++){
-            double sqrt=Math.sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
-            double n1=points[i][0];
-            double n2=points[i][1];
-            pq.add(new double[]{n1,n2,sqrt});      
+            double srt=Math.sqrt(points[i][0]*points[i][0] + points[i][1]*points[i][1]);
+            double x=points[i][0];
+            double y=points[i][1];
+            pq.add(new double[]{x,y,srt});
         }
         int i=0;
         while(k-->0){
             double[] arr=pq.poll();
-            int n1=(int)arr[0];
-            int n2=(int)arr[1];
-            ans[i][0]=n1;
-            ans[i][1]=n2;
-            i++;
+            ans[i][0]=(int)arr[0];
+            ans[i++][1]=(int)arr[1];
         }
         return ans;
     }
