745db223f6e3c259e711f7beab5ad3965700e988
Anashva Singh <anashva2406@gmail.com>
2025-11-11 20:34:39 +0530

    Time: 118 ms (27.62%) | Memory: 68.1 MB (5.59%) - LeetSync

---
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int bestTeamScore(int[] scores, int[] ages) {
+        int n=scores.length;
+        int[][] arr=new int[n][2];
+        for(int i=0;i<n;i++){
+            arr[i][0]=ages[i];
+            arr[i][1]=scores[i];
+        }
+        Arrays.sort(arr,(a,b)->{
+            if(a[0]==b[0]){
+                return a[1]-b[1];
+            }
+            return a[0]-b[0];
+        });
+
+        int[][] dp=new int[scores.length][scores.length];
+        for(int[] a:dp){
+            Arrays.fill(a,-1);
+        }
+        return max(arr,0,dp,-1);
+
+    }
+    public int max(int[][] arr,int idx,int[][] dp,int j){
+            if(idx>=arr.length){
+                return 0;
+            }
+            if(dp[j+1][idx]!=-1){
+                return dp[j+1][idx];
+            }
+            int pick=0;
+            if(j==-1 || arr[j][1]<=arr[idx][1]){
+                pick=arr[idx][1]+max(arr,idx+1,dp,idx);
+            }
+            int npick=max(arr,idx+1,dp,j);
+            return dp[j+1][idx]=Math.max(pick,npick);
+      }
+}
