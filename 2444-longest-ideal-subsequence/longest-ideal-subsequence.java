287baa0a949dc8a829b0c4e264978858c459b1eb
Anashva Singh <anashva2406@gmail.com>
2025-11-24 22:44:14 +0530

    Time: 465 ms (5.03%) | Memory: 302.9 MB (5.03%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.java
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.java
@@ -1,10 +1,10 @@
 class Solution {
     public int longestIdealString(String s, int k) {
-        int[][] dp=new int[s.length()+1][150];
+        int[][] dp=new int[s.length()+1][150];// 150 isliye liye h ki char ki ascii value max 150 tk ho skta h
         for(int[]a:dp){
             Arrays.fill(a,-1);
         }
-        return longest(0,s,k,dp,'#');
+        return longest(0,s,k,dp,'#');//# taken because prevously no character is there for dummy taken
     }
     public int longest(int idx,String s,int k,int[][] dp,char ch){
         if(idx>=s.length()){
@@ -14,7 +14,7 @@ class Solution {
             return dp[idx][ch];
         }
         int ans=0;
-        if(Math.abs(ch-s.charAt(idx))<=k || ch=='#'){
+        if(Math.abs(ch-s.charAt(idx))<=k || ch=='#'){//1 char ko hmesa allow krega lene k liye
             ans=1+longest(idx+1,s,k,dp,s.charAt(idx));
         }
         int np=longest(idx+1,s,k,dp,ch);
