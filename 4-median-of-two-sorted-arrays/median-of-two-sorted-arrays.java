910827803365df7267d3c2dc03b403fb51840273
Anashva Singh <anashva2406@gmail.com>
2025-12-18 23:34:29 +0530

    Time: 7 ms (21.35%) | Memory: 48.9 MB (36.56%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,38 +1,21 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1.length>nums2.length){
-            return findMedianSortedArrays(nums2,nums1);
+        int n=nums1.length;
+        int m=nums2.length;
+        int[] nums=new int[n+m];
+        int j=0;
+        for(int i=0;i<n;i++){
+            nums[j++]=nums1[i];
         }
-        int l=0;
-        int h=nums1.length;
-        int total=nums1.length+nums2.length;
-        int hf=(total+1)/2;
-        while(l<=h){
-            int al=(l+h)/2;
-            int bl=hf-al;
-
-            int maxal=(al==0) ? Integer.MIN_VALUE:nums1[al-1];
-            int minal=(al==nums1.length) ? Integer.MAX_VALUE :nums1[al];
-            int maxbl=(bl==0) ? Integer.MIN_VALUE : nums2[bl-1];
-            int minbl=(bl==nums2.length) ? Integer.MAX_VALUE:nums2[bl];
-
-            if(maxal<=minbl && maxbl<=minal){
-                double ans=0;
-                if(total%2==0){
-                    ans=(Math.max(maxal,maxbl)+Math.min(minbl,minal))/2.0;
-                }
-                else{
-                    ans=Math.max(maxal,maxbl)/1.0;
-                }
-                return ans;
-            }
-            else if(maxbl>minal){
-                l=al+1;
-            }
-            else {
-                h=al-1;
-            }
+        for(int i=0;i<m;i++){
+            nums[j++]=nums2[i];
         }
-        return 0.0;
+        Arrays.sort(nums);
+        if((nums.length)%2==0){
+            return (double)(nums[nums.length/2]+nums[nums.length/2-1])/2;
+        }
+        else{
+            return (double)nums[nums.length/2];
+        }
     }
 }
