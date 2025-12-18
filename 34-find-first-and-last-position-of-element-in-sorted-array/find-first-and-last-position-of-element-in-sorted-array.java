ba630fdac9c6218998b0e8ce74e0c28972a7ae61
Anashva Singh <anashva2406@gmail.com>
2025-12-18 22:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (15.48%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,77 +1,41 @@
 class Solution {
-//     public int[] searchRange(int[] arr, int target) {
-//         // int start=-1;
-//         // int end=-1;
-//         // for(int i=0;i<nums.length;i++){
-//         //     if(nums[i]==target){
-//         //         if(start==-1){
-//         //             start=i;
-//         //         }
-//         //         end=i;
-//         //     }
-//         // }
-//         // return new int[]{start,end};
-
-//         int l=0;
-//         int h=arr.length-1;
-//         while(l<=h){
-//             int mid=l+(h-l)/2;
-//             if(arr[mid]==target){
-//                 int n=mid;
-//                 while(mid>0 && arr[mid-1]==target){//ye loop hmara lower index ko check krega target k
-//                     mid--;
-//                 }
-//                 while(n<arr.length-1 && arr[n+1]==target){//ye loop hmara upper bound ko check krega target k
-//                     n++;
-//                 }
-//                 return new int[]{mid,n};
-//             }
-//             else if(arr[mid]<target){
-//                 l=mid+1;
-//             }
-//             else{
-//                 h=mid-1;
-//             }
-
-//         }
-//         return new int[]{-1,-1};
-//    }
-
-
-    public int[] searchRange(int[] arr, int target) {
-        int first = First(arr, target);
-        int last = Last(arr, target);
-        return new int[]{first, last};
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{first(nums,target),last(nums,target)};
     }
-
-    public int First(int[] arr, int target) {
-        int l = 0, h = arr.length - 1;
-        int ans = -1;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            if (arr[mid] == target) {
-                ans = mid;
-                h = mid - 1; 
-            } else if (arr[mid] < target) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
+    public int last(int[] num,int target){
+        int l=0;
+        int h=num.length-1;
+        int ans=-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(num[mid]==target){
+                ans=mid;
+                l=mid+1;
+            }
+            else if(num[mid]<target){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
             }
         }
         return ans;
     }
-    public int Last(int[] arr, int target) {
-        int l = 0, h = arr.length - 1;
-        int ans = -1;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            if (arr[mid] == target) {
-                ans = mid;
-                l = mid + 1;
-            } else if (arr[mid] < target) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
+    public int first(int[] num,int target){
+        int l=0;
+        int h=num.length-1;
+        int ans=-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(num[mid]==target){
+                ans=mid;
+                h=mid-1;
+            }
+            else if(num[mid]<target){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
             }
         }
         return ans;
