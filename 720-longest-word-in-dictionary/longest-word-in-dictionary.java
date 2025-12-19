b0e47e01b94c31f996aee07955a60dcf93368c82
Anashva Singh <anashva2406@gmail.com>
2025-12-19 21:48:41 +0530

    Time: 20 ms (19.09%) | Memory: 46.8 MB (37.15%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.java
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -1,18 +1,21 @@
 class Solution {
     public String longestWord(String[] words) {
-        HashSet<String> st=new HashSet<>();
         Arrays.sort(words,(a,b)->{
-            if(a.equals(b)){
-                return 0;
-            }
             if(a.compareTo(b)!=0){
                 return a.compareTo(b);
             }
-            return Integer.compare(b.length(),a.length());
+            return Integer.compare(a.length(),b.length());
         });
         String ans="";
+        HashSet<String> st=new HashSet<>();
         for(String s:words){
-            if(s.length()==1 || st.contains(s.substring(0,s.length()-1))){
+            if(s.length()==1){
+                st.add(s);
+                if(s.length()>ans.length()){
+                    ans=s;
+                }
+            }
+            if(st.contains(s.substring(0,s.length()-1))){
                 st.add(s);
                 if(s.length()>ans.length()){
                     ans=s;
