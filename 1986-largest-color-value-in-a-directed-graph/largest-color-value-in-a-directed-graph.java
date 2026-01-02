b8e6124b12d651f4babde8df98a08eeb06fd376c
Anashva Singh <anashva2406@gmail.com>
2026-01-02 16:15:03 +0530

    Time: 96 ms (32.04%) | Memory: 176.3 MB (35.56%) - LeetSync

---
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.java
@@ -0,0 +1,53 @@
+class Solution {
+    int[][] dp;
+    HashMap<Integer,List<Integer>> mp;
+    public int largestPathValue(String s, int[][] edges) {
+        int n =s.length();
+        dp=new int[n][26];
+        int[] indegree=new int[n];
+        mp=new HashMap<>();
+        for(int i=0;i<n;i++){
+            mp.put(i,new ArrayList<>());
+        }
+        for(int[] e:edges){
+            int u=e[0];
+            int v=e[1];
+            mp.get(u).add(v);
+            indegree[v]++;
+        }
+        for(int i=0;i<n;i++){
+            int ch=s.charAt(i)-'a';
+            dp[i][ch]=1;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        int ans=0;
+        int c=0;
+        while(!q.isEmpty()){
+            int r=q.poll();
+            c++;
+            for(int i=0;i<26;i++){
+                ans=Math.max(ans,dp[r][i]);
+            }
+            for(int nbr:mp.get(r)){
+                for(int i=0;i<26;i++){
+                    int ch=s.charAt(nbr)-'a';
+                    int d=0;
+                    if(ch==i){
+                        d=1;
+                    }
+                    dp[nbr][i]=Math.max(dp[r][i]+d,dp[nbr][i]);
+                }
+                indegree[nbr]--;
+                if(indegree[nbr]==0){
+                    q.add(nbr);
+                }
+            }
+        }
+        return c<n ? -1 : ans;
+    }
+}
