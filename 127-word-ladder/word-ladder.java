dace890ec57d25fbebde1ed4c549161e968c1ed8
Anashva Singh <anashva2406@gmail.com>
2026-01-09 15:54:23 +0530

    Time: 93 ms (40.58%) | Memory: 49 MB (37.14%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,15 +1,15 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        return count(beginWord,endWord,wordList);
+        return ladder(beginWord,endWord,wordList);
     }
-    public int count(String src,String des,List<String> ll){
+    public int ladder(String src,String des,List<String> ll){
         Queue<pair> q=new LinkedList<>();
         HashSet<String> st=new HashSet<>();
-        q.add(new pair(src,1));
         for(String s:ll){
             st.add(s);
         }
         st.remove(src);
+        q.add(new pair(src,1));
         while(!q.isEmpty()){
             pair rp=q.poll();
             String s=rp.s;
@@ -21,22 +21,21 @@ class Solution {
                 for(char ch='a';ch<='z';ch++){
                     char[] arr=s.toCharArray();
                     arr[i]=ch;
-                    String news=new String(arr);
-                    if(st.contains(news)){
-                        st.remove(news);
-                        q.add(new pair(news,step+1));
+                    String ns=new String(arr);
+                    if(st.contains(ns)){
+                        st.remove(ns);
+                        q.add(new pair(ns,step+1));
                     }
                 }
             }
         }
         return 0;
     }
-
     class pair{
-        String s;
         int step;
-        pair(String s,int step){
-            this.s=s;
+        String s;
+        public pair(String s,int step){
+            this.s=s;    
             this.step=step;
         }
     }
