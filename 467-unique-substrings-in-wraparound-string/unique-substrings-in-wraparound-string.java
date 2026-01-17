ee9c60c958434c20e015f9d82f778e6576076ec6
Anashva Singh <anashva2406@gmail.com>
2026-01-18 00:33:20 +0530

    Time: 10 ms (45.42%) | Memory: 45.2 MB (23.90%) - LeetSync

--- 467-unique-substrings-in-wraparound-string/unique-substrings-in-wraparound-string.java
+++ 467-unique-substrings-in-wraparound-string/unique-substrings-in-wraparound-string.java
@@ -1,20 +1,20 @@
 class Solution {
     public int findSubstringInWraproundString(String s) {
-        int[] count=new int[26];
+        int[] ch=new int[26];
         int c=1;
         for(int i=0;i<s.length();i++){
-            if (i > 0 && ((s.charAt(i) - s.charAt(i - 1) == 1) || (s.charAt(i - 1) - s.charAt(i) == 25))) {
+            if(i>0 && (s.charAt(i)-s.charAt(i-1)==1 || s.charAt(i-1)-s.charAt(i)==25)){
                 c++;
             }
             else{
                 c=1;
             }
-            int id=s.charAt(i)-'a';
-            count[id]=Math.max(c,count[id]);
+            int idx=s.charAt(i)-'a';// valid substring upto given end
+            ch[idx]=Math.max(c,ch[idx]);
         }
         int ans=0;
-        for(int i:count){
-            ans+=i;
+        for(int k:ch){
+            ans+=k;
         }
         return ans;
     }
