b7dba951520d0448d2e15b614675883fe9fdeb32
Anashva Singh <anashva2406@gmail.com>
2026-01-24 10:17:45 +0530

    Time: 5 ms (15.11%) | Memory: 42.9 MB (79.74%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.java
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -1,19 +1,20 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
-        int[] arr1=new int[nums.length];
-        int[] arr2=new int[nums.length];
-        Arrays.fill(arr1,1);
-        Arrays.fill(arr2,1);
-        for(int i=1;i<arr1.length;i++){
-            for(int j=i-1;j>=0;j--){
-                if(nums[i]>nums[j]){
-                    arr1[i]=Math.max(arr1[i],arr2[j]+1);
+        int n=nums.length;
+        int[] dp1=new int[n];
+        int[] dp2=new int[n];
+        Arrays.fill(dp1,1);
+        Arrays.fill(dp2,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]>nums[i]){
+                    dp1[i]=Math.max(dp1[i],dp2[j]+1);
                 }
-                if(nums[i]<nums[j]){
-                    arr2[i]=Math.max(arr2[i],arr1[j]+1);
+                if(nums[j]<nums[i]){
+                    dp2[i]=Math.max(dp2[i],dp1[j]+1);
                 }
             }
         }
-        return Math.max(arr1[arr1.length-1],arr2[arr2.length-1]);
+        return Math.max(dp1[n-1],dp2[n-1]);
     }
 }
