ea9fc4f697e963ef899f72108712b54ef9c35708
Anashva Singh <anashva2406@gmail.com>
2026-01-24 15:37:50 +0530

    Time: 44 ms (41.89%) | Memory: 81.1 MB (6.54%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.java
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.java
@@ -1,3 +1,37 @@
+// class Solution {
+//     public int longestSubsequence(int[] arr, int difference) {
+//         int n = arr.length;
+//         int[][] dp = new int[n][n + 1];
+//         for (int[] row : dp) {
+//             Arrays.fill(row, -1);
+//         }
+//         return subs(arr, 0, -1, difference, dp);
+//     }
+
+//     public int subs(int[] arr, int idx, int prevIdx, int diff, int[][] dp) {
+//         if (idx == arr.length) {
+//             return 0;
+//         }
+
+//         if (dp[idx][prevIdx + 1] != -1) {
+//             return dp[idx][prevIdx + 1];
+//         }
+
+//         int take = 0;
+//         int skip;
+
+//         if (prevIdx == -1 || arr[idx] - arr[prevIdx] == diff) {
+//             take = 1 + subs(arr, idx + 1, idx, diff, dp);
+//         }
+
+//         skip = subs(arr, idx + 1, prevIdx, diff, dp);
+
+//         return dp[idx][prevIdx + 1] = Math.max(take, skip);
+//     }
+// }
+
+
+
 class Solution {
     public int longestSubsequence(int[] arr, int difference) {
         HashMap<Integer,Integer> mp=new HashMap<>();
