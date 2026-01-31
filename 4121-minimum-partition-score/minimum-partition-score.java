47cf67086b02df1145dd4634cf2f142588fefb22
Anashva Singh <anashva2406@gmail.com>
2026-02-01 02:06:49 +0530

    Time: 1410 ms (0.00%) | Memory: 98.9 MB (0.00%) - LeetSync

--- 4121-minimum-partition-score/minimum-partition-score.java
+++ 4121-minimum-partition-score/minimum-partition-score.java
@@ -3,7 +3,6 @@ class Solution {
 
     public long minPartitionScore(int[] nums, int k) {
         int n = nums.length;
-
         prefix = new long[n + 1];
         for (int i = 0; i < n; i++) {
             prefix[i + 1] = prefix[i] + nums[i];
@@ -38,7 +37,7 @@ class Solution {
         return dp[k][n];
     }
 
-    static class Line {
+    class Line {
         long m, c;
         double xLeft;
         Line(long m, long c) {
@@ -46,14 +45,16 @@ class Solution {
             this.c = c;
             this.xLeft = Double.NEGATIVE_INFINITY;
         }
-        long value(long x) { return m * x + c; }
+        long value(long x) { 
+            return m * x + c; 
+        }
     }
 
-    private double intersect(Line l1, Line l2) {
+    public double intersect(Line l1, Line l2) {
         return (double)(l2.c - l1.c) / (l1.m - l2.m);
     }
 
-    private void addLine(Deque<Line> hull, long m, long c) {
+    public void addLine(Deque<Line> hull, long m, long c) {
         Line newLine = new Line(m, c);
         while (!hull.isEmpty()) {
             Line last = hull.peekLast();
@@ -65,16 +66,22 @@ class Solution {
                 break;
             }
         }
-        if (hull.isEmpty()) newLine.xLeft = Double.NEGATIVE_INFINITY;
+        if (hull.isEmpty()){
+            newLine.xLeft = Double.NEGATIVE_INFINITY;
+        } 
         hull.addLast(newLine);
     }
 
-    private long query(Deque<Line> hull, long x) {
+    public long query(Deque<Line> hull, long x) {
         while (hull.size() >= 2) {
             Line first = hull.peekFirst();
-            Line second = hull.toArray(new Line[0])[1];
-            if (second.xLeft <= x) hull.pollFirst();
-            else break;
+            Line second = hull.stream().skip(1).findFirst().get();
+            if (second.xLeft <= x) {
+                hull.pollFirst();
+            }
+            else {
+                break;
+            }
         }
         return hull.peekFirst().value(x);
     }
