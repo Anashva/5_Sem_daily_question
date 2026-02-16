1ffca61e75687801dfb81d195a4cf37805b17773
Anashva Singh <anashva2406@gmail.com>
2026-02-16 12:41:40 +0530

    Time: 2 ms (63.97%) | Memory: 46.5 MB (37.56%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,59 +13,25 @@
  *     }
  * }
  */
-// class Solution {
-//     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        // int[] preindex={0};
-        // return tree(preorder,inorder,0,preorder.length-1,preindex);
-    // }
-    // public TreeNode tree(int[] preorder,int[] inorder,int left,int right,int[]preindex){
-    //     if(left>right){
-    //         return null;
-    //     }
-    //     int rootval=preorder[preindex[0]];
-    //     preindex[0]++;
-    //     TreeNode root=new TreeNode(rootval);
-    //     int index=search(inorder,left,right,rootval);
-
-    //     root.left=tree(preorder,inorder,left,index-1,preindex);
-    //     root.right=tree(preorder,inorder,index+1,right,preindex);
-
-    //     return root;
-    // }
-    // public int search(int[] inorder,int left,int right,int val){
-    //     for(int i=left;i<=right;i++){
-    //         if(val==inorder[i]){
-    //             return i;
-    //         }
-    //     }
-    //     return -1;
-    // }
-
-// }
-
-
 class Solution {
+    HashMap<Integer,Integer> mp;
+    int pre=0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return createTree(inorder,preorder,0,inorder.length-1,0,preorder.length-1);
+        mp=new HashMap<>();
+        for(int i=0;i<inorder.length;i++){
+            mp.put(inorder[i],i);
+        }
+        return tree(preorder,0,preorder.length-1);
     }
-    public TreeNode createTree(int[] in,int [] pre,int ilo,int ihi,int plo,int phi){
-        if(ilo>ihi || plo>phi){
+    public TreeNode tree(int[] preo,int si,int ei){
+        if(si>ei){
             return null;
         }
-        TreeNode node=new TreeNode(pre[plo]);
-        int idx=search(in,ilo,ihi,pre[plo]);
-        int c=idx-ilo;
-        node.left=createTree(in,pre,ilo,idx-1,plo+1,plo+c);
-        node.right=createTree(in,pre,idx+1,ihi,plo+c+1,phi);
-        return node;
-
-    }
-    public int search(int[]in,int ilo,int ihi,int item){
-        for(int i=ilo;i<=ihi;i++){
-            if(in[i]==item){
-                return i;
-            }
-        }
-        return -1;
+        int val=preo[pre++];
+        TreeNode root=new TreeNode(val);
+        int idx=mp.get(val);
+        root.left=tree(preo,si,idx-1);
+        root.right=tree(preo,idx+1,ei);
+        return root;
     }
 }
