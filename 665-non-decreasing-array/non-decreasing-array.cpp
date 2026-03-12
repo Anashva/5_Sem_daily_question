572c063b9e540add01a0e8f41dc2c7f50e49b79a
Anashva Singh <anashva2406@gmail.com>
2026-03-12 18:20:36 +0530

    Time: 0 ms (100.00%) | Memory: 30.7 MB (74.89%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        return check(nums);
+    }
+    bool check(vector<int> &nums){
+        int n=nums.size();
+        int c=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[i-1]){
+                c++;
+                if(c>1){
+                    return false;
+                }
+                if(i==1 || nums[i]>=nums[i-2]){
+                    nums[i-1]=nums[i];
+                }
+                else{
+                    nums[i]=nums[i-1];
+                }
+
+            }
+        }
+        return true;
+        
+    }
+};
