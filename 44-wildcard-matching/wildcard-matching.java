3781c0648089033f5e2f60708e2235440ad700ec
Anashva Singh <anashva2406@gmail.com>
2026-03-30 15:50:42 +0530

    Time: 100 ms (5.11%) | Memory: 63.9 MB (7.89%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,38 +1,36 @@
 class Solution {
+    Boolean[][] dp;
     public boolean isMatch(String s, String p) {
-        Boolean[][] dp=new Boolean[s.length()][p.length()];
-        for(Boolean[] a:dp ){
-            Arrays.fill(a,null);
-        }
-        return match(s, p, 0, 0,dp);
+        dp=new Boolean[s.length()][p.length()];
+        return match(s,p,0,0);
     }
-
-    public boolean match(String s, String p, int i, int j,Boolean[][]dp) {
-        if (i == s.length() && j == p.length()) {
+    public boolean match(String s,String p,int i,int j){
+        if(i==s.length() && j==p.length()){
             return true;
         }
-        if (j == p.length()) {
+        if(j==p.length()){
             return false;
         }
-
-        if (i == s.length()) {
-            for (int k = j; k < p.length(); k++) {
-                if (p.charAt(k) != '*') return false;
+        if(i==s.length()){
+            for(int k=j;k<p.length();k++){
+                if(p.charAt(k)!='*'){
+                    return false;
+                }
             }
             return true;
         }
         if(dp[i][j]!=null){
             return dp[i][j];
         }
-        boolean flag = false;
-        boolean f2 = false;
 
-        if (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j)) {
-            f2 = match(s, p, i + 1, j + 1,dp);
-        } else if (p.charAt(j) == '*') {
-            flag = match(s, p, i, j + 1,dp) || match(s, p, i + 1, j,dp);
+        boolean f1=false;
+        boolean f2=false;
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
+            f1=match(s,p,i+1,j+1);
         }
-
-        return dp[i][j]=flag || f2;
+        else if(p.charAt(j)=='*'){
+            f2=match(s,p,i,j+1) || match(s,p,i+1,j);
+        }
+        return dp[i][j]= f1 || f2;
     }
 }
