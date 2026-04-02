7b96bfdc063c9b6663259a8ed5fbbd726a3260e2
Anashva Singh <anashva2406@gmail.com>
2026-04-02 10:25:56 +0530

    Time: 111 ms (12.86%) | Memory: 136.7 MB (31.74%) - LeetSync

--- 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
+++ 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
-    unordered_map<long long ,int> dp;
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
-        return count(nums1,nums2,-1,-1,0);
+        int n=nums1.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return count(nums1,nums2,0,0,dp);
     }
-    long long make(int prev1,int prev2,int idx){
-        return ((long long)(prev1+1) << 42) |((long long)(prev2+1) << 21) | idx;
-    }
-    int count(vector<int>& nums1,vector<int>&nums2,int prev1,int prev2,int idx){
+    int count(vector<int>& nums1,vector<int> &nums2,int idx,int swap,vector<vector<int>> &dp){
         if(idx==nums1.size()){
             return 0;
         }
-        long long key=make(prev1,prev2,idx);
-        if(dp.find(key)!=dp.end()){
-            return dp[key];
+        if(dp[idx][swap]!=-1){
+            return dp[idx][swap];
         }
-        int ans=INT_MAX;
-        if((prev1==-1 && prev2==-1) || (nums1[idx]>prev1 && nums2[idx]>prev2)){
-            ans=count(nums1,nums2,nums1[idx],nums2[idx],idx+1);
+        int prev1=-1;
+        int prev2=-1;
+        if(swap==1){
+            prev1=nums2[idx-1];
+            prev2=nums1[idx-1];
         }
-        if((prev1==-1 && prev2==-1) || (prev1<nums2[idx] && prev2<nums1[idx])){
-            ans=min(ans,1+count(nums1,nums2,nums2[idx],nums1[idx],idx+1));
+        else if(idx>0){
+            prev1=nums1[idx-1];
+            prev2=nums2[idx-1];
         }
-        return dp[key]=ans;
+        int swapp=INT_MAX;
+        int noswap=INT_MAX;
+        if(prev1<nums1[idx] && prev2<nums2[idx]){
+            noswap=count(nums1,nums2,idx+1,0,dp);//no swap
+        }
+        if(prev1<nums2[idx] && prev2<nums1[idx]){
+            swapp=1+count(nums1,nums2,idx+1,1,dp);//swapped
+        }
+        return dp[idx][swap]=min(swapp,noswap);
     }
+
 };
