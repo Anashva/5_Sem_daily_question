8ba7c8e7b96bf1db43583089ab7c7f1a17508582
Anashva Singh <anashva2406@gmail.com>
2026-05-28 23:18:13 +0530

    Time: 124 ms (5.89%) | Memory: 90.1 MB (5.02%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        unordered_map<int,unordered_map<int,double>> mp;
+        for(int i=0;i<edges.size();i++){
+            mp[edges[i][0]][edges[i][1]]=succProb[i];
+            mp[edges[i][1]][edges[i][0]]=succProb[i];
+        }
+        priority_queue<pair<double,int>> pq;
+        pq.push({1.0,start});
+        set<int> st;
+        while(!pq.empty()){
+            auto[cost,vtx]=pq.top();
+            pq.pop();
+            if(vtx==end){
+                return cost;
+            }
+            if(st.count(vtx)){
+                continue;
+            }
+            st.insert(vtx);
+            for(auto &e: mp[vtx]){
+                int nbr=e.first;
+                double ncost=e.second*cost;
+                if(!st.count(nbr)){
+                    pq.push({ncost,nbr});
+                }
+            }
+        }
+        return 0.0;
+    }
+};
