a2d10978e1d658f72a0b7332edb52aeb310c8cd4
Anashva Singh <anashva2406@gmail.com>
2026-05-28 23:23:41 +0530

    Time: 70 ms (15.09%) | Memory: 73.7 MB (18.83%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        unordered_map<int,unordered_map<int,double>> mp;
+        // unordered_map<int,unordered_map<int,double>> mp;
+        // for(int i=0;i<edges.size();i++){
+        //     mp[edges[i][0]][edges[i][1]]=succProb[i];
+        //     mp[edges[i][1]][edges[i][0]]=succProb[i];
+        // }
+
+        vector<vector<pair<int,double>>> ll(n);
         for(int i=0;i<edges.size();i++){
-            mp[edges[i][0]][edges[i][1]]=succProb[i];
-            mp[edges[i][1]][edges[i][0]]=succProb[i];
+            int u=edges[i][0];
+            int v=edges[i][1];
+            double wt=succProb[i];
+            ll[u].push_back({v,wt});
+            ll[v].push_back({u,wt});
         }
         priority_queue<pair<double,int>> pq;
         pq.push({1.0,start});
@@ -19,7 +28,7 @@ public:
                 continue;
             }
             st.insert(vtx);
-            for(auto &e: mp[vtx]){
+            for(auto &e: ll[vtx]){
                 int nbr=e.first;
                 double ncost=e.second*cost;
                 if(!st.count(nbr)){
