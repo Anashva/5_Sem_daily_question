3753ebd22c197943094d3e416431f74e5d73394b
Anashva Singh <anashva2406@gmail.com>
2026-07-03 12:10:55 +0530

    Time: 27 ms (65.51%) | Memory: 110.7 MB (56.42%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.cpp
+++ 2725-mice-and-cheese/mice-and-cheese.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
-        priority_queue<int,vector<int>, greater<int>> pq;
+        // priority_queue<int,vector<int>, greater<int>> pq;
+        // int total=0;
+        // for(int i=0;i<reward1.size();i++){
+        //     total+=reward2[i];
+        //     int diff=reward1[i]-reward2[i];
+        //     pq.push(diff);
+        //     if(pq.size()>k){
+        //         pq.pop();
+        //     }
+        // }
+        // while(!pq.empty()){
+        //     total+=pq.top();
+        //     pq.pop();
+        // }
+
+        int n=reward1.size();
+        vector<int> diff;
         int total=0;
-        for(int i=0;i<reward1.size();i++){
+        for(int i=0;i<n;i++){
             total+=reward2[i];
-            int diff=reward1[i]-reward2[i];
-            pq.push(diff);
-            if(pq.size()>k){
-                pq.pop();
-            }
+            diff.push_back(reward1[i]-reward2[i]);
         }
-        while(!pq.empty()){
-            total+=pq.top();
-            pq.pop();
+        sort(diff.begin(),diff.end(),greater<int>());
+        for(int i=0;i<k;i++){
+            total+=diff[i];
         }
         return total;
     }
