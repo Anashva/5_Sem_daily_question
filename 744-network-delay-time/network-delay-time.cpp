9b1db1f69e10ff31e5d8d9661e795db14eac0b3b
Anashva Singh <anashva2406@gmail.com>
2026-07-07 23:16:16 +0530

    Time: 85 ms (51.06%) | Memory: 44.3 MB (82.57%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int,unordered_map<int,int>> mp;
-        for(auto &e: times){
-            int u=e[0];
-            int v=e[1];
-            int wt=e[2];
-            mp[u][v]=wt;
+        vector<vector<pair<int,int>>> ll(n+1);
+        for(auto &e:times){
+            ll[e[0]].push_back({e[1],e[2]});
         }
         vector<int> dis(n+1,INT_MAX);
+        dis[k]=0;
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         pq.push({0,k});
-        dis[k]=0;
         while(!pq.empty()){
-            auto rp=pq.top();
+            auto[cost,vtx]=pq.top();
             pq.pop();
-            int wt=rp.first;
-            int vtx=rp.second;
-            if(dis[vtx]<wt){
+            if(dis[vtx]<cost){
                 continue;
             }
-            dis[vtx]=wt;
-            for(auto &e: mp[vtx]){
-                int cost=e.second+wt;
+            dis[vtx]=cost;
+            for(auto &e:ll[vtx]){
                 int nbr=e.first;
-                if(dis[nbr]>cost){
-                    dis[nbr]=cost;
-                    pq.push({cost,nbr});
+                int ncost=cost+e.second;
+                if(dis[nbr]>ncost){
+                    dis[nbr]=ncost;
+                    pq.push({ncost,nbr});
                 }
             }
         }
@@ -38,5 +33,6 @@ public:
             ans=max(ans,dis[i]);
         }
         return ans;
+
     }
 };
