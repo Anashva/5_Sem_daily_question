fcd3438c35ebf18ae1ea6c30c3539691d58119e7
Anashva Singh <anashva2406@gmail.com>
2026-08-10 20:11:23 +0530

    Time: 3 ms (74.67%) | Memory: 28.4 MB (65.02%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        vector<int> ans(nums.size(),-1);
+        int n=nums.size();
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums[i%n]){
+                st.pop();
+            }
+            if(i<n && !st.empty()){
+                ans[i%n]=st.top();
+            }
+            st.push(nums[i%n]);
+        }
+        return ans;
+    }
+};
+
+// using monotonic decreasing stack
+// we will pop element from stack until we get grreater element that 
