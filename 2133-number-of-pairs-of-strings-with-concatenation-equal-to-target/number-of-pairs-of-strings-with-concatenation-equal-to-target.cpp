5fd7f41f6a331c3a333a6d958c501540b38826cc
Anashva Singh <anashva2406@gmail.com>
2026-08-31 01:12:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (91.77%) - LeetSync

--- 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string targt) {
-        int c=0;
+        // int c=0;
+        // for(int i=0;i<nums.size();i++){
+        //     string s=nums[i];
+        //     for(int j=0;j<nums.size();j++){
+        //         if(i==j){
+        //             continue;
+        //         }
+        //         string t=nums[j]+s;
+        //         if(t==targt){
+        //             c++;
+        //         }
+        //     }
+        // }
+        // return c;
+
+        unordered_map<string,int> mp;
+        int ans=0;
         for(int i=0;i<nums.size();i++){
-            string s=nums[i];
-            for(int j=0;j<nums.size();j++){
-                if(i==j){
-                    continue;
-                }
-                string t=nums[j]+s;
-                if(t==targt){
-                    c++;
+            if(targt.starts_with(nums[i])){
+                string s=targt.substr(nums[i].size());
+                if(mp.count(s)){
+                    ans+=mp[s];
                 }
             }
+            if(targt.ends_with(nums[i])){
+                string s=targt.substr(0,targt.size()-nums[i].size());
+                if(mp.count(s)){
+                    ans+=mp[s];
+                }
+            }
+            mp[nums[i]]++;
+            
         }
-        return c;
+        return ans;
     }
 };   
